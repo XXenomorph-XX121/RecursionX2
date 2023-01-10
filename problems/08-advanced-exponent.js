0e2bb5fccd6643392bd25c24dd963338e3b7c66b
Serghei Mineev <mineevserghei@gmail.com>
2023-01-10 17:19:38 -0500

    did all of them

--- problems/08-advanced-exponent.js
+++ problems/08-advanced-exponent.js
@@ -23,12 +23,12 @@ Examples:
 advancedExponent(2, 0); // 1
 advancedExponent(2, 1); // 2
 advancedExponent(2, 2); // 4
-advancedExponent(2, 3); // 8
+advancedExponent(2, 3); // 8 2**5
 advancedExponent(2, 4); // 16
-advancedExponent(2, 5); // 32
+advancedExponent(2, 5); // 32       2^2.5 * 2^2.5
 advancedExponent(2, 6); // 64
-advancedExponent(2, 7); // 128
-advancedExponent(2, 8); // 256
+advancedExponent(2, 7); // 128      2^(7/2)
+advancedExponent(2, 8); // 256      2^4 * 2^4
 advancedExponent(2, 9); // 512
 advancedExponent(2, 10); // 1024
 advancedExponent(2, 11); // 2048
@@ -41,9 +41,34 @@ times `advancedExponent` is being recursively called.
 
 
 function advancedExponent(b, n) {
-  // Your code here
+
+  if (n === 0) return 1;
+
+  if (n === 1) return b;
+
+  if (n % 2 === 0) { //even
+    return advancedExponent(b, (n / 2)) * advancedExponent(b, (n / 2));
+  } else {  // odd
+    return b * (advancedExponent(b, (n - 1) / 2) * advancedExponent(b, (n - 1) / 2))
+  }
+
 }
 
+
+
+
+// if(power > 0) {
+//   if(power === 1) {
+//       return num;
+//   }
+//   return num * exponent(num, power - 1)
+// }
+// if (power < 0) {
+//   if(power === -1) {
+//       return 1/num;
+//   }
+//   return 1/num * exponent(num, power + 1)
+// }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
